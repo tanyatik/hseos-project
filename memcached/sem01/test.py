01776d185ae39f4f83f929b799d8995dc8da842f
Tatiana Borisova <tsborisova@tsborisova.i.fog.yandex.net>
2015-10-18 05:15:45 +0300

    Add more tests, add socket shutdown

--- memcached/sem01/test.py
+++ memcached/sem01/test.py
@@ -1,14 +1,36 @@
 import socket
 import sys
 
-port = int(sys.argv[1])
+port = 3249
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("localhost", port))
 
-msg = "hello!"
+def check_echo(msg):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("localhost", port))
 
-s.send("hello!")
-res = s.recv(len(msg))
+    s.send(msg)
+    s.shutdown(socket.SHUT_WR)  # break reading cycle in echo server
+    res = s.recv(len(msg))
+    assert msg == res
 
-assert msg == res
+
+def test_hello():
+    msg = "hello!"
+    check_echo(msg)
+
+
+def test_one_byte():
+    msg = "1"
+    check_echo(msg)
+
+
+def test_big_message():
+    msg = "BIGDATA" * 1000
+    check_echo(msg)
+
+
+if __name__ == "__main__":
+    port = int(sys.argv[1])
+    test_hello()
+    test_one_byte()
+    test_big_message()
